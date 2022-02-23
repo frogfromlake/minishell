d7085da4b6b9c204bcece3c60eb34ae4a7e73a7a
FQ <Nelix@posteo.de>
2022-02-23 17:24:19 +0100

    added input check and pwd built-in

--- src/main.c
+++ src/main.c
@@ -6,12 +6,32 @@
 /*   By: fquist <fquist@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 16:20:25 by dmontema          #+#    #+#             */
-/*   Updated: 2022/02/23 16:50:05 by fquist           ###   ########.fr       */
+/*   Updated: 2022/02/23 17:21:57 by fquist           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <unistd.h>
+
+static int	check_whitespace(char c)
+{
+	if (c == ' ' || c == '\n' || c == '\t')
+		return (1);
+	return (0);
+}
+
+static int	check_empty_input(char *input)
+{
+	int	i;
+
+	i = 0;
+	while (check_whitespace(input[i]))
+		i++;
+	if (input[i] == '\0')
+		return (1);
+	else
+		return (0);
+}
 
 static char	*get_prompt(void)
 {
@@ -30,19 +50,23 @@ static void	bitchy_snake_shell(t_cmd **head)
 	while (true)
 	{
 		read = get_prompt();
-		if (read && !ft_strcmp(read, ""))
+		if (read != NULL && !ft_strcmp(read, ""))
 		{
 			add_history(read);
+			if (!check_empty_input(read))
+			{
+				// do stuff
+			}
 		}
-		if (!read || !ft_strcmp(read, "exit"))
+		if (read == NULL || !ft_strcmp(read, "exit"))
 		{
 			write(1, "logout\n", 8);
 			exit(EXIT_SUCCESS);
 		}
-		if (!ft_strcmp(read, "pwd"))
-		{
-			printf("%s\n", get_cwd());
-		}
+		// if (!ft_strcmp(read, "pwd"))
+		// {
+		// 	printf("%s\n", get_cwd());
+		// }
 	}
 }
 
