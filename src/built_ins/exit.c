8a2812230d1bcae898e025c2829ac18a1b08f0bc
Denice Montemayor <dmontema@1-C-7.42heilbronn.de>
2022-03-28 22:53:52 +0200

    exit: handled error cases and returns right exit status.

--- src/built_ins/exit.c
+++ src/built_ins/exit.c
@@ -12,13 +12,54 @@
 
 #include "../../include/minishell.h"
 
+static int	exit_error(char *str, int r_value)
+{
+	t_stringbuilder	*sb;
+	char			*err_msg;
+
+	sb = sb_create();
+	sb_append_str(sb, "exit: ");
+	sb_append_str(sb, str);
+	sb_append_str(sb, ": numeric argument required");
+	err_msg = sb_get_str(sb);
+	sb_destroy(sb);
+	error_msg(err_msg, r_value);
+	free(err_msg);
+	return (r_value);
+}
+
+static bool	check_valid_arg(char *arg)
+{
+	if ((arg[0] == '+' || arg[0] == '-') && ft_is_digit(arg[1]))
+		return (true);
+	else
+		return (false);
+}
+
 void	ft_exit(t_table *table)
 {
+	unsigned char	exit_status;
+
 	if (!ft_strcmp(table->exe, "exit"))
 	{
-		// free stuff
-		write(1, "exit\n", 5);
-		free_table(&table);
-		exit(g_exit_status);
+		if (check_valid_arg(table->args))
+		{
+			exit_status = ft_atoi(table->args);
+			write(1, "exit\n", 5);
+			g_exit_status = exit_status;
+			exit(g_exit_status);
+		}
+		else
+		{
+			if (ft_strrchr(table->args, ' '))
+				g_exit_status = error_msg("exit: too many arguments", FAIL);
+			else
+			{
+				write(1, "exit\n", 5);
+				g_exit_status = exit_error(table->args, 255);
+				free_table(&table);
+				exit(g_exit_status);
+			}
+		}
 	}
 }
