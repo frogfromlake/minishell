1786e025525e719b05793bbe90a5a9531b941103
Denice Montemayor <dmontema@1-D-7.42heilbronn.de>
2022-04-04 17:06:09 +0200

    built-in: echo: adjusted to recent changes from struct.

--- src/built_ins/echo.c
+++ src/built_ins/echo.c
@@ -12,10 +12,70 @@
 
 #include "../../include/minishell.h"
 
+int	check_empty_arg(char *input)
+{
+	int	i;
+
+	i = 0;
+	while (check_whitespace(input[i]))
+		i++;
+	if (input[i] == '\0')
+		return (1);
+	else
+		return (0);
+}
+
+static bool	check_valid_opt(char *opt)
+{
+	int	i;
+
+	i = 0;
+	if (ft_strncmp(opt, "-n", 2))
+		return (false);
+	i += 2;
+	while (opt[i])
+	{
+		if (opt[i] != 'n')
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+static int	skip_valid_opt(char **args, bool *opt)
+{
+	int	i;
+
+	i = 0;
+	if (!check_valid_opt(args[i]))
+		return (i);
+	*opt = true;
+	i++;
+	while (args[i])
+	{
+		if (!check_valid_opt(args[i]) && ft_strcmp(args[i], ""))
+			break ;
+		i++;
+	}
+	return (i);
+}
+
 void	ft_echo(t_table *table)
 {
-	if (table->args)
-		printf("%s", table->args);
-	if (!table->opt)
+	bool	opt;
+	int		i;
+
+	opt = false;
+	i = skip_valid_opt(table->cmd_arr, &opt);
+	while (table->cmd_arr[i])
+	{
+		if (!check_empty_arg(table->cmd_arr[i]))
+			printf("%s", table->cmd_arr[i]);
+		if (ft_strcmp(table->cmd_arr[i], "")
+			&& table->cmd_arr[i + 1] && ft_strcmp(table->cmd_arr[i + 1], ""))
+			printf(" ");
+		i++;
+	}
+	if (!opt)
 		printf("\n");
 }
